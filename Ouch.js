48c81c82db278526be1f5077885946773665fe14
Harish Anchu <harishanchu@wimoku.com>
2015-02-24 13:23:03 +0530

    Implemented methods in Ouch main script

--- Ouch.js
+++ Ouch.js
@@ -2,5 +2,139 @@
  * Ouch.js
  *
  * @author: Harish Anchu <twitter:@harishanchu>
- * Copyright (c) 2015, Wimoku Pvt Ltd. All rights reserved.
+ * Copyright (c) 2015, Harish Anchu. All rights reserved.
  */
+
+var Inspector = require('./exception/Inspector');
+var _ = require('lodash');
+var PrettyPageHandler = require('./handler/PrettyPageHandler');
+var Handler = require('./handler/Handler');
+
+/**
+ * @param {Array} handlerStack
+ * @constructor
+ */
+function Ouch(handlerStack) {
+    /**
+     * Stores error handlers
+     * @var {Array} handlerStack
+     * @protected
+     */
+    this.__handlerStack = handlerStack || [];
+}
+
+/**
+ * Error handler classes
+ *
+ * @type {{PrettyPageHandler: (PrettyPageHandler)}}
+ */
+Ouch.handlers = {
+    BaseHandler: Handler,
+    PrettyPageHandler: PrettyPageHandler
+};
+
+/**
+ * Returns an array with all handlers, in the order they were added to the stack.
+ *
+ * @returns {Array}
+ */
+Ouch.prototype.getHandlers = function () {
+    return this.__handlerStack;
+};
+
+/**
+ * Pushes a handler to the end of the stack.
+ *
+ * @throws TypeError  If argument is not callable or instance of HandlerInterface
+ * @param handler
+ * @returns {Ouch}
+ */
+Ouch.prototype.pushHandler = function (handler) {
+    if (_.isFunction(handler) || _.isFunction(handler.handle)) {
+        this.__handlerStack.push(handler);
+    } else {
+        throw new TypeError("Argument must be a callable, or object should have method 'handle'")
+    }
+    return this;
+};
+
+/**
+ * Removes the last handler in the stack and returns it.
+ * Returns undefined if there"s nothing else to pop.
+ * @returns {function|Object|undefined}
+ */
+Ouch.prototype.popHandler = function () {
+    return this.__handlerStack.pop();
+};
+
+/**
+ * Clears all handlers in the handlerStack.
+ *
+ * @returns {Ouch}
+ */
+Ouch.prototype.clearHandlers = function () {
+    this.__handlerStack = [];
+    return this;
+};
+
+/**
+ * @param  {Error|String|Object} exception
+ * @returns Inspector
+ * @protected
+ */
+Ouch.prototype.__getInspector = function (exception) {
+    return new Inspector(exception);
+};
+
+/**
+ * Handles an exception, ultimately generating a Ouch error
+ * page. Responses from each handler will be pushed into an array
+ * and will be sent along to the callback if present as first argument.
+ *
+ * @param  {Error} exception
+ * @param  {function} CB
+ * @param  {Object} request
+ * @param  {Object} response
+ */
+Ouch.prototype.handleException = function (exception, request, response, CB) {
+    if (!request) {
+        request = null;
+    }
+
+    if (!response) {
+        response = null;
+    }
+
+    if (!CB) {
+        CB = null;
+    }
+
+    (function (inspector, handlerStack, self) {
+        var output = [];
+
+        function next(handleResponse) {
+            output.push(handleResponse);
+            if (handlerStack.length) {
+                var handler = handlerStack.pop();
+
+                if (handler instanceof Handler) {
+                    handler.setRun(self);
+                    handler.setInspector(inspector);
+                    handler.setRequest(request);
+                    handler.setResponse(response);
+                    handler.handle(next, exception);
+                } else {
+                    handler(next, exception, request, response)
+                }
+            } else {
+                if (CB) {
+                    CB(output.slice(1));
+                }
+            }
+        }
+
+        next();
+    })(this.__getInspector(exception), this.__handlerStack.slice(0), this)
+};
+
+module.exports = Ouch;
