e9e3a5374e97e10a6e45d0f913b547b7598fe07f
Harish Anchu <harishanchu@wimoku.com>
2015-03-05 22:40:04 +0530

    Unit tests for PrettyPageHandler and TemplateHelper

---
+++ test/unit/handler/prettyPageHandler.spec.js
@@ -0,0 +1,69 @@
+/**
+ * prettyPageHandler.spec.js
+ *
+ * @author: Harish Anchu <twitter:@harishanchu>
+ * Copyright (c) 2015, Wimoku Pvt Ltd. All rights reserved.
+ */
+
+var Ouch = require('../../../Ouch');
+var PrettyPageHandler = require('../../../handler/PrettyPageHandler');
+
+describe("PrettyPageHandler", function(){
+    /**
+     * Test that PrettyPageHandler handles the template without
+     * any errors.
+     * @covers PrettyPageHandler#handle
+     */
+   describe("#handle()", function(){
+        it("should handle exception without errors", function(done){
+           var ouch = new Ouch([new PrettyPageHandler()]);
+            ouch.handleException(new Error("Test error foo"), null, null, function(response){
+                // Reached the end without errors
+                response[0].should.be.ok;
+                done();
+            })
+        });
+   });
+
+    /**
+     * covers #getEditorHref()
+     */
+    describe("#setEditor()", function(){
+        var handler = new PrettyPageHandler();
+        it("should set handlers editor to the newly supplied", function(done){
+
+            handler.setEditor('sublime');
+            handler.getEditorHref('/foo/bar.js', 10).should.equal('subl://open?url=file://%2Ffoo%2Fbar.js&line=10');
+            handler.getEditorHref('/foo/with space?.js', 2324).should.equal('subl://open?url=file://%2Ffoo%2Fwith%20space%3F.js&line=2324');
+            handler.getEditorHref('/foo/bar/with-dash.js', 0).should.equal('subl://open?url=file://%2Ffoo%2Fbar%2Fwith-dash.js&line=0');
+
+            handler.setEditor(function (file, line) {
+                file = encodeURIComponent(file);
+                line = encodeURIComponent(line);
+                return "http://google.com/search/?q="+ file + ":" + line;
+            });
+
+            handler.getEditorHref('/foo/bar.js', 10).should.equal('http://google.com/search/?q=%2Ffoo%2Fbar.js:10');
+
+            done();
+        })
+    });
+
+    describe("#addEditor()", function(){
+        var handler = new PrettyPageHandler();
+        it("should add new editor to the handler", function(done){
+            handler.addEditor('test', function (file, line) {
+               return "cool beans " + file + ":" + line;
+            });
+
+            handler.setEditor('test');
+            handler.getEditorHref("hello", 20).should.equal("cool beans hello:20");
+
+            done();
+        });
+    });
+
+    //@todo: Add remaining cases
+
+
+});
---
+++ test/unit/util/templateHelper.spec.js
@@ -0,0 +1,63 @@
+/**
+ * templateHelper.spec.js
+ *
+ * @author: Harish Anchu <twitter:@harishanchu>
+ * Copyright (c) 2015, Wimoku Pvt Ltd. All rights reserved.
+ */
+
+var TemplateHelper = require('../../../util/TemplateHelper');
+var path = require('path');
+
+describe('TemplateHelper', function(){
+    var helper = new TemplateHelper();
+
+    /**
+     * @covers #getVAriables()
+     */
+    describe("#setVariables()", function(){
+        it("should set template variables which should be available with getVariables method", function(done){
+            var variables = {
+                name: "Ouch",
+                type: "node module"
+            };
+
+            helper.setVariables(variables);
+
+            helper.getVariables().should.be.equal(variables);
+
+            done();
+        });
+    });
+
+
+    describe("#render()", function(){
+        it("should render and ejs template file.", function(done){
+            var template = path.join(__dirname, "../../fixtures/template.ejs");
+
+            helper.render(template, {"name": "B<o>b"}).should.be.equal("hello-world\nMy name is B&lt;o&gt;b");
+
+            done();
+        });
+    });
+
+    describe("#slug()", function(){
+        it("should convert a string to slug version", function(done){
+            "hello-world".should.be.equal(helper.slug("Hello, world!"));
+            "potato-class".should.be.equal(helper.slug("Potato class"));
+            done();
+        })
+    });
+
+    describe("#escapeButPreserveUris()", function(){
+       it("should html escape string but preseve url's and convert it to clickable anchor elements", function(done){
+           var original = "This is a <a href=''>http://google.com</a> test string";
+
+           helper.escapeButPreserveUris(original)
+               .should.be
+               .equal("This is a &lt;a href=&#39;&#39;&gt;<a href=\"http://google.com\" target=\"_blank\">http://google.com</a>&lt;/a&gt; test string");
+
+           done();
+       });
+    });
+
+});
