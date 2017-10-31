c93dbcb8e9e6d78ac26b03c649d726ddf3f6b466
kevin viglucci <kviglucci@gmail.com>
2017-10-30 22:07:00 -0500

    update all strings to use single quotes for consistency

--- test/unit/exception/frame.spec.js
+++ test/unit/exception/frame.spec.js
@@ -8,22 +8,22 @@
 
 /** do not move this comment from this location - used for testing**/
 
-var Inspector = require("../../../exception/Inspector");
-var frame = require("../../../exception/frame");
+var Inspector = require('../../../exception/Inspector');
+var frame = require('../../../exception/frame');
 var fs = require('fs');
 
 describe('frame', function () {
-    var inspector = new Inspector(new Error("Sample exception message foo"));
+    var inspector = new Inspector(new Error('Sample exception message foo'));
 
-    describe("#getFileLines()", function () {
-        it("should return content of specified line numbers of the stack frame file source", function (done) {
-            inspector.getFrames()[0].getFileLines(8, 1).should.containEql("/** do not move this comment from this location - used for testing**/");
+    describe('#getFileLines()', function () {
+        it('should return content of specified line numbers of the stack frame file source', function (done) {
+            inspector.getFrames()[0].getFileLines(8, 1).should.containEql('/** do not move this comment from this location - used for testing**/');
             done();
         });
     });
 
-    describe("#getFileContents()", function () {
-        it("should returns the full contents of the file for the frame", function (done) {
+    describe('#getFileContents()', function () {
+        it('should returns the full contents of the file for the frame', function (done) {
             inspector.getFrames()[0].getFileContents().should.containEql(fs.readFileSync(__filename, 'utf-8'));
             done();
         });
@@ -33,7 +33,7 @@ describe('frame', function () {
      * covers #addComment()
      */
     describe('#getComments()', function () {
-        it("should get comments set to frame by addComment method", function (done) {
+        it('should get comments set to frame by addComment method', function (done) {
             var frame = inspector.getFrames()[0];
 
             var testComments = [
