c75ede9abd9882b0b2615d7d3d58d9ac4d791971
kevin viglucci <kviglucci@gmail.com>
2017-10-30 22:08:35 -0500

    update quotes to use double quotes for consistency with project code style

--- test/unit/exception/frame.spec.js
+++ test/unit/exception/frame.spec.js
@@ -8,23 +8,23 @@
 
 /** do not move this comment from this location - used for testing**/
 
-var Inspector = require('../../../exception/Inspector');
-var frame = require('../../../exception/frame');
-var fs = require('fs');
+var Inspector = require("../../../exception/Inspector");
+var frame = require("../../../exception/frame");
+var fs = require("fs");
 
-describe('frame', function () {
-    var inspector = new Inspector(new Error('Sample exception message foo'));
+describe("frame", function () {
+    var inspector = new Inspector(new Error("Sample exception message foo"));
 
-    describe('#getFileLines()', function () {
-        it('should return content of specified line numbers of the stack frame file source', function (done) {
-            inspector.getFrames()[0].getFileLines(8, 1).should.containEql('/** do not move this comment from this location - used for testing**/');
+    describe("#getFileLines()", function () {
+        it("should return content of specified line numbers of the stack frame file source", function (done) {
+            inspector.getFrames()[0].getFileLines(8, 1).should.containEql("/** do not move this comment from this location - used for testing**/");
             done();
         });
     });
 
-    describe('#getFileContents()', function () {
-        it('should returns the full contents of the file for the frame', function (done) {
-            inspector.getFrames()[0].getFileContents().should.containEql(fs.readFileSync(__filename, 'utf-8'));
+    describe("#getFileContents()", function () {
+        it("should returns the full contents of the file for the frame", function (done) {
+            inspector.getFrames()[0].getFileContents().should.containEql(fs.readFileSync(__filename, "utf-8"));
             done();
         });
     });
@@ -32,14 +32,14 @@ describe('frame', function () {
     /**
      * covers #addComment()
      */
-    describe('#getComments()', function () {
-        it('should get comments set to frame by addComment method', function (done) {
+    describe("#getComments()", function () {
+        it("should get comments set to frame by addComment method", function (done) {
             var frame = inspector.getFrames()[0];
 
             var testComments = [
-                'Dang, yo!',
-                'Errthangs broken!',
-                'Dayumm!'
+                "Dang, yo!",
+                "Errthangs broken!",
+                "Dayumm!"
             ];
 
             frame.addComment(testComments[0]);
@@ -50,9 +50,9 @@ describe('frame', function () {
 
             comments.should.have.length(3);
 
-            comments[0]['comment'].should.equal(testComments[0]);
-            comments[1]['comment'].should.equal(testComments[1]);
-            comments[2]['comment'].should.equal(testComments[2]);
+            comments[0]["comment"].should.equal(testComments[0]);
+            comments[1]["comment"].should.equal(testComments[1]);
+            comments[2]["comment"].should.equal(testComments[2]);
 
             done();
         });
